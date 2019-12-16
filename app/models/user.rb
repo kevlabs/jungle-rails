7320427dbd0dbdd60d4df48d02b2b664422af002
Kevin Laburte <kevin.laburte@gmail.com>
2019-12-16 14:14:38 -0500

    Add Rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: { message: "can't be blank" }
+  validates :email, presence: true
   validates_uniqueness_of :email, allow_blank: true, message: ": Umm, did you mean to log in??"
   has_secure_password
 
