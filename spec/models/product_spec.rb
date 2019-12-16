f16b42a97d3870358358a16582051899a4291fd5
Kevin Laburte <kevin.laburte@gmail.com>
2019-12-16 14:43:13 -0500

    Update tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,11 +17,9 @@ RSpec.describe Product, type: :model do
     end
     
     it "is not valid without a price" do
-      # subject.price_cents = nil
-      # expect(subject).to_not be_valid
-      product = Product.new(name: "Anything", quantity: 2, category: category)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Price can't be blank")
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Price can't be blank")
     end
     
     it "is not valid without a quantity" do
